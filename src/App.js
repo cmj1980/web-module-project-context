8f6336b42f01defb6420ee20eeb33fdcf1b8670b
cmj1980 <dagoclmt@gmail.com>
2022-05-19 14:55:46 -0700

    ProductContext added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartConext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -24,10 +27,11 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
-
+            </ProductContext.Provider>
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
